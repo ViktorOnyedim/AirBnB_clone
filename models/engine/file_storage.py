26cfef5a23f9904007307725eb36409be3933b72
ViktorOnyedim <viktoronyedim@gmail.com>
2023-03-12 00:05:37 +0000

    Modified file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,12 +7,12 @@ class FileStorage:
     """
     Serializes instances to a JSON file and deserializes JSON file to instances
     """
-    __file_path = "objects.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns the dictionary objects"""
-        returns self.__objects
+        return self.__objects
 
     def new(self, obj):
         """
