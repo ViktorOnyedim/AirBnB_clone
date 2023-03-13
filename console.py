227dd68fc2c6659d02040c7378f904c869551af2
ViktorOnyedim <viktoronyedim@gmail.com>
2023-03-13 03:29:52 +0000

    Implemented the create() method

--- console.py
+++ console.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Console Program"""
 import cmd
+import json
+from models.engine.file_storage import FileStorage
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """Command Interpreter for HBNB project"""
     prompt = '(hbnb) '
-
+    
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
@@ -19,6 +22,36 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on empty input line."""
         pass
 
+    def help_quit(self):
+        """Help message for the quit command"""
+        print("Exit the command interpreter")
+
+    def help_EOF(self):
+        """Help message for the EOF command"""
+        print("Exit the command interpreter")
+
+    def do_create(self, arg):
+        """Create a new instance of a BaseModel subclass."""
+        if not arg:
+            print("** class name missing **")
+        else:
+            try:
+                cls = models.getattr(models, arg)
+                obj = cls()
+                obj.save()
+                print(obj.id)
+            except AttributeError:
+                print("** class doesn't exist **")
+
+    def help_create(self):
+        """Help message for the create command"""
+        print("Create a new instance of BaseModel")
+        print("Usage: create <class_name>")
+
+    def show(self, arg):
+        """S"""
+        if not arg:
+            print("** class name missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
