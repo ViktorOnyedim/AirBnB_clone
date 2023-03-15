49e3157dcc6366e2950a8494ee225a2d8d3a8aa9
ViktorOnyedim <viktoronyedim@gmail.com>
2023-03-15 00:37:08 +0000

    pycodestyle fix

--- models/user.py
+++ models/user.py
@@ -13,8 +13,7 @@ class User(BaseModel):
     - first_name: string - empty string
     - last_name: string - empty string
     """
-    email: str  = ""
+    email: str = ""
     password: str = ""
     first_name: str = ""
     last_name: str = ""
-
