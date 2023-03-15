c5ad751c6265c5fa4a325d9cffdefd0fdb5577fc
ViktorOnyedim <viktoronyedim@gmail.com>
2023-03-15 01:12:27 +0000

    Modified file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,10 +53,20 @@ class FileStorage:
     def classes(cls):
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
         # import other models here"""
 
         return {
             "BaseModel": BaseModel,
             "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": import Review,
             # add other models here
         }
